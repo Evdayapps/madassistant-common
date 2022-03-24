82c3798f9698507fd335f67705a43850adcd0f88
Shannon Rodricks <shanrod220@gmail.com>
2022-03-24 12:33:15 +0530

    ADDED: Support to redact headers in api call logs UPDATED: Version code to 13

--- madassistant-common/src/main/java/com/evdayapps/madassistant/common/models/permissions/ApiCallsPermissionModel.kt
+++ madassistant-common/src/main/java/com/evdayapps/madassistant/common/models/permissions/ApiCallsPermissionModel.kt
@@ -1,26 +1,32 @@
 package com.evdayapps.madassistant.common.models.permissions
 
 import com.evdayapps.madassistant.common.kotlinx.getStringOrNull
+import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
 data class ApiCallsPermissionModel(
     val enabled: Boolean = false,
     val filterMethod: String? = null,
-    val filterUrl: String? = null
+    val filterUrl: String? = null,
+    val redactHeaders: List<String>? = null
 ) {
 
     companion object {
         private const val KEY_enabled = "enabled"
         private const val KEY_filterMethod = "filterMethod"
         private const val KEY_filterUrl = "filterUrl"
+        private const val KEY_redactHeaders = "redactHeaders"
     }
 
     @Throws(JSONException::class)
     constructor(json: JSONObject) : this(
         enabled = json.optBoolean(KEY_enabled, false),
         filterMethod = json.getStringOrNull(KEY_filterMethod),
-        filterUrl = json.getStringOrNull(KEY_filterUrl)
+        filterUrl = json.getStringOrNull(KEY_filterUrl),
+        redactHeaders = json.getJSONArray(KEY_redactHeaders)?.let {
+            List(it.length()) { index -> it.getString(index) }
+        }
     )
 
     fun toJsonObject(): JSONObject {
@@ -28,6 +34,13 @@ data class ApiCallsPermissionModel(
             put(KEY_enabled, enabled)
             putOpt(KEY_filterMethod, filterMethod)
             putOpt(KEY_filterUrl, filterUrl)
+            if (!redactHeaders.isNullOrEmpty()) {
+                val array = JSONArray()
+                for (header in redactHeaders) {
+                    array.put(header)
+                }
+                putOpt(KEY_redactHeaders, array)
+            }
         }
     }
 }
