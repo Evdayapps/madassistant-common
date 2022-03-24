5d1c48681f3abc49ac7dfff49ec664a44c25c165
Shannon Rodricks <shanrod220@gmail.com>
2022-03-24 11:30:15 +0530

    ADDED: Constants for intent extras keys UPDATED: Version code

--- madassistant-common/src/main/java/com/evdayapps/madassistant/common/MADAssistantConstants.kt
+++ madassistant-common/src/main/java/com/evdayapps/madassistant/common/MADAssistantConstants.kt
@@ -2,6 +2,9 @@ package com.evdayapps.madassistant.common
 
 object MADAssistantConstants {
     const val AIDLVersion = 1
+
+    const val KEY_EXTRA_INFO = "info"
+    const val KEY_EXTRA_AIDL_VERSION = "aidlVersion"
 }
 
 
