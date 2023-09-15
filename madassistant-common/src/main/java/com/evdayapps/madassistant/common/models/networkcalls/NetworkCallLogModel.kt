3d6b254ec7b3a3f6715f882fed029ded662877f6
Shannon Rodricks <shanrod220@gmail.com>
2023-09-15 12:02:22 +0530

    Split Network Model into request,response,options,error models

--- madassistant-common/src/main/java/com/evdayapps/madassistant/common/models/networkcalls/NetworkCallLogModel.kt
+++ madassistant-common/src/main/java/com/evdayapps/madassistant/common/models/networkcalls/NetworkCallLogModel.kt
@@ -1,118 +1,162 @@
 package com.evdayapps.madassistant.common.models.networkcalls
 
+
 import com.evdayapps.madassistant.common.models.exceptions.ExceptionModel
 import org.json.JSONArray
 import org.json.JSONObject
 
-/**
- * // Request
- * val requestHeaders : Map<String></String>, String>,
- * val requestMethod : String,
- * val requestTimestamp : Long,
- * val requestUrl : String,
- * val requestBody: Map<String></String>, *>,
- * // Response
- * val responseHeaders : Map<String></String>, String>,
- * val responseStatusCode: Int,
- * val responseTimestamp : Long,
- * val response : String
- */
+// Model representing a network call, including options, request, response, and exception details
 data class NetworkCallLogModel(
-    var threadName: String? = null,
-    var connectTimeoutMillis: Int? = null,
-    var readTimeoutMillis: Int? = null,
-    var writeTimeoutMillis: Int? = null,
-    var protocol: String? = null,
-    var method: String = "",
-    var url: String = "",
-    var requestHeaders: JSONArray? = null,
-    var requestTimestamp: Long = 0,
-    var requestBody: String? = null,
-    var responseHeaders: JSONArray? = null,
-    var responseStatusCode: Int? = null,
-    var responseTimestamp: Long = 0,
-    var gzippedLength: Long? = null,
-    var responseLength: Long? = null,
-    var responseBody: String? = null,
-    var exception: ExceptionModel? = null
+    var options: Options? = null, // Connection options
+    var request: Request? = null, // Request details
+    var response: Response? = null, // Response details
+    var exception: ExceptionModel? = null // Exception details (if any)
 ) {
-
-    companion object {
-        private const val KEY_threadName = "threadName"
-        private const val KEY_protocol = "protocol"
-        const val KEY_method = "method"
-        const val KEY_url = "url"
-
-        // Request
-        private const val KEY_connect_timeout_ms = "connect_timeout_ms"
-        private const val KEY_read_timeout_ms = "read_timeout_ms"
-        private const val KEY_write_timeout_ms = "write_timeout_ms"
-        const val KEY_request_timestamp = "request_timestamp"
-        const val KEY_request_headers = "request_headers"
-        const val KEY_request_body = "request_body"
-
-        // Response
-        const val KEY_response_headers = "response_headers"
-        const val KEY_response_status_code = "response_status"
-        const val KEY_response_timestamp = "response_timestamp"
-        const val KEY_response_body = "response_body"
-        private const val KEY_response_length = "response_length"
-        private const val KEY_response_gzipped_length = "response_gzipped_length"
-
-        // Exception
-        private const val KEY_exception = "exception"
-    }
-
-    @Throws(Exception::class)
-    constructor(json: JSONObject) : this(
-        threadName = json.getString(KEY_threadName),
-        protocol = json.optString(KEY_protocol, null),
-        method = json.optString(KEY_method, null),
-        url = json.optString(KEY_url, null),
-
-        // Request
-        connectTimeoutMillis = json.getInt(KEY_connect_timeout_ms),
-        readTimeoutMillis = json.getInt(KEY_read_timeout_ms),
-        writeTimeoutMillis = json.getInt(KEY_write_timeout_ms),
-        requestTimestamp = json.getLong(KEY_request_timestamp),
-        requestHeaders = json.getJSONArray(KEY_request_headers),
-        requestBody = json.optString(KEY_request_body),
-
-        // Response
-        responseTimestamp = json.getLong(KEY_response_timestamp),
-        responseStatusCode = json.getInt(KEY_response_status_code),
-        responseHeaders = json.optJSONArray(KEY_response_headers),
-        responseBody = json.optString(KEY_response_body),
-        responseLength = json.optLong(KEY_response_length),
-        gzippedLength = json.optLong(KEY_response_gzipped_length),
-
-        // Exception
-        exception = json.optJSONObject(KEY_exception)?.run { ExceptionModel(this) }
-    )
-
-    fun toJsonObject(): JSONObject {
+    fun toJson(): JSONObject {
         return JSONObject().apply {
-            put(KEY_threadName, threadName)
-            put(KEY_method, method)
-            put(KEY_url, url)
-
-            put(KEY_connect_timeout_ms, connectTimeoutMillis)
-            put(KEY_read_timeout_ms, readTimeoutMillis)
-            put(KEY_write_timeout_ms, writeTimeoutMillis)
-
-            put(KEY_request_timestamp, requestTimestamp)
-            put(KEY_request_headers, requestHeaders)
-            put(KEY_request_body, requestBody)
-
-            put(KEY_response_status_code, responseStatusCode)
-            put(KEY_response_timestamp, responseTimestamp)
-            put(KEY_response_body, responseBody)
-            put(KEY_response_headers, responseHeaders)
-            put(KEY_response_length, responseLength)
-
-            putOpt(KEY_response_gzipped_length, gzippedLength)
-            putOpt(KEY_exception, exception?.toJsonObject())
+            put("options", options?.toJson())
+            put("request", request?.toJson())
+            put("response", response?.toJson())
+            put("exception", exception?.toJsonObject()) // Assuming ExceptionModel has toJson
         }
     }
 
+    companion object {
+        fun fromJson(json: JSONObject): NetworkCallLogModel {
+            return NetworkCallLogModel(
+                options = json.optJSONObject("options")?.let { Options.fromJson(it) },
+                request = json.optJSONObject("request")?.let { Request.fromJson(it) },
+                response = json.optJSONObject("response")?.let { Response.fromJson(it) },
+                exception = json.optJSONObject("exception")
+                    ?.let { ExceptionModel(it) } // Assuming ExceptionModel has fromJson
+            )
+        }
+    }
+}
+
+// Options for the network call, including timeouts and handshake details
+data class Options(
+    var threadName: String? = null, // Name of the thread handling the call
+    var connectTimeoutMillis: Int? = null, // Connection timeout in milliseconds
+    var readTimeoutMillis: Int? = null, // Read timeout in milliseconds
+    var writeTimeoutMillis: Int? = null, // Write timeout in milliseconds
+    var protocol: String? = null, // Protocol used (e.g., HTTP/1.1)
+    var handshake: Handshake? = null // SSL/TLS handshake details
+) {
+    fun toJson(): JSONObject {
+        return JSONObject().apply {
+            put("threadName", threadName)
+            put("connectTimeoutMillis", connectTimeoutMillis)
+            put("readTimeoutMillis", readTimeoutMillis)
+            put("writeTimeoutMillis", writeTimeoutMillis)
+            put("protocol", protocol)
+            put("handshake", handshake?.toJson())
+        }
+    }
+
+    companion object {
+        fun fromJson(json: JSONObject): Options {
+            return Options(
+                threadName = json.optString("threadName"),
+                connectTimeoutMillis = json.optInt("connectTimeoutMillis"),
+                readTimeoutMillis = json.optInt("readTimeoutMillis"),
+                writeTimeoutMillis = json.optInt("writeTimeoutMillis"),
+                protocol = json.optString("protocol"),
+                handshake = json.optJSONObject("handshake")?.let { Handshake.fromJson(it) }
+            )
+        }
+    }
+}
+
+// Details of the SSL/TLS handshake
+data class Handshake(
+    var protocolVersion: String? = null, // SSL/TLS protocol version
+    var cipherSuite: String? = null // Cipher suite used for encryption
+) {
+    fun toJson(): JSONObject {
+        return JSONObject().apply {
+            put("protocolVersion", protocolVersion)
+            put("cipherSuite", cipherSuite)
+        }
+    }
+
+    companion object {
+        fun fromJson(json: JSONObject): Handshake {
+            return Handshake(
+                protocolVersion = json.optString("protocolVersion"),
+                cipherSuite = json.optString("cipherSuite")
+            )
+        }
+    }
+}
+
+// Details of the HTTP request
+data class Request(
+    var method: String = "", // HTTP method (e.g., GET, POST)
+    var url: String = "", // URL of the request
+    var headers: JSONArray? = null, // HTTP headers
+    var timestamp: Long = 0, // Timestamp of the request
+    var contentType: String? = null,
+    var body: String? = null // Request body
+) {
+    fun toJson(): JSONObject {
+        return JSONObject().apply {
+            put("method", method)
+            put("url", url)
+            put("headers", headers)
+            put("timestamp", timestamp)
+            put("contentType", contentType)
+            put("body", body)
+        }
+    }
+
+    companion object {
+        fun fromJson(json: JSONObject): Request {
+            return Request(
+                method = json.optString("method"),
+                url = json.optString("url"),
+                headers = json.optJSONArray("headers"),
+                timestamp = json.optLong("timestamp"),
+                contentType = json.optString("contentType"),
+                body = json.optString("body")
+            )
+        }
+    }
+}
+
+// Details of the HTTP response
+data class Response(
+    var headers: JSONArray? = null, // HTTP headers
+    var statusCode: Int? = null, // HTTP status code
+    var timestamp: Long = 0, // Timestamp of the response
+    var gzippedLength: Long? = null, // Length of gzipped response
+    var length: Long? = null, // Length of response
+    var contentType: String? = null,
+    var body: String? = null // Response body
+) {
+    fun toJson(): JSONObject {
+        return JSONObject().apply {
+            put("headers", headers)
+            put("statusCode", statusCode)
+            put("timestamp", timestamp)
+            put("gzippedLength", gzippedLength)
+            put("length", length)
+            put("contentType", contentType)
+            put("body", body)
+        }
+    }
+
+    companion object {
+        fun fromJson(json: JSONObject): Response {
+            return Response(
+                headers = json.optJSONArray("headers"),
+                statusCode = json.optInt("statusCode"),
+                timestamp = json.optLong("timestamp"),
+                gzippedLength = json.optLong("gzippedLength"),
+                length = json.optLong("length"),
+                contentType = json.optString("contentType"),
+                body = json.optString("body")
+            )
+        }
+    }
 }
